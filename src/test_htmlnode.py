b47134a737626e56284c2b737b6b92be20a87f83
Client4894 <lucas.richers@pikoworks.de>
2024-04-06 17:40:00 +0200

    Added LeafNode + Tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,17 +1,7 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
-    def test_repr(self):
-        node = HTMLNode(
-            "p", 
-            "text inside the paragraph", 
-            ["node1", "node2"], 
-            {'href': 'https://www.google.com', 'target': '_blank'})
-        self.assertEqual(
-            "HTMLNode(p, text inside the paragraph, children: ['node1', 'node2'], {'href': 'https://www.google.com', 'target': '_blank'})", repr(node)
-        )
-
     def test_prop_to_html(self):
         node = HTMLNode(
             "p", 
@@ -21,6 +11,14 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(
             ' href="https://www.google.com" target="_blank"', node.props_to_html()
         )
+
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
 
 if __name__ == "__main__":
     unittest.main()
